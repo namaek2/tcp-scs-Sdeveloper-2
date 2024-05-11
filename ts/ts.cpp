050c432a440b62664c2b51c4c3485e4953d15413
namaek_2 <science4588@gmail.com>
2024-05-11 18:14:00 +0900

    과제끝ㅌㅌㅌㅌㅌ

--- ts/ts.cpp
+++ ts/ts.cpp
@@ -18,18 +18,22 @@ void myerror(const char* msg) { fprintf(stderr, "%s %ld\n", msg, GetLastError())
 void myerror(const char* msg) { fprintf(stderr, "%s %s %d\n", msg, strerror(errno), errno); }
 #endif
 
+#define MAX_CLIENT 5
+
 void usage() {
 	printf("tcp server %s\n",
 #include "../version.txt"
    );
 	printf("\n");
-	printf("syntax: ts <port> [-e] [-si <src ip>]\n");
+	printf("syntax: ts <port> [-e[-b]] [-si <src ip>]\n");
 	printf("  -e : echo\n");
-	printf("sample: ts 1234\n");
+    printf("  -b : broadcast\n");
+	printf("sample: ts 1234 -e -b\n");
 }
 
 struct Param {
 	bool echo{false};
+    bool broad_cast{false};
 	uint16_t port{0};
 	uint32_t srcIp{0};
 
@@ -40,6 +44,12 @@ struct Param {
 				i++;
 				continue;
 			}
+
+            if (strcmp(argv[i], "-b") == 0) {
+                broad_cast = true;
+                i++;
+                continue;
+            }
 
 			if (strcmp(argv[i], "-si") == 0) {
 				int res = inet_pton(AF_INET, argv[i + 1], &srcIp);
@@ -58,8 +68,26 @@ struct Param {
 	}
 } param;
 
-void recvThread(int sd) {
-	printf("connected\n");
+void recvThread(int sd, int sd_arr[], int *sd_i) {
+    if(*sd_i == MAX_CLIENT) {
+        printf("MAX_CLIENT\n");
+        printf("refused\n");
+        fflush(stdout);
+        ::close(sd);
+    }
+
+    for(int i=0; i < MAX_CLIENT; i++) {
+        if(sd_arr[i] == -1) {
+            sd_arr[i] = sd;
+            *sd_i+=1;
+            break;
+        }
+    }
+
+    printf("sd_i: %d\n", *sd_i);
+    printf("sd_arr[%d]: %d\n", *sd_i - 1, sd_arr[*sd_i - 1]);
+
+	printf("connected %d\n", sd);
 	fflush(stdout);
 	static const int BUFSIZE = 65536;
 	char buf[BUFSIZE];
@@ -73,17 +101,30 @@ void recvThread(int sd) {
 		buf[res] = '\0';
 		printf("%s", buf);
 		fflush(stdout);
+
 		if (param.echo) {
-			res = ::send(sd, buf, res, 0);
-			if (res == 0 || res == -1) {
-				fprintf(stderr, "send return %ld", res);
-				myerror(" ");
-				break;
-			}
+            if (param.broad_cast) {
+                for(int i=0; i<MAX_CLIENT; i++) {
+                    if (sd_arr[i] != -1) {
+                        res = ::send(sd_arr[i], buf, res, 0);
+                        if (res == 0 || res == -1) {
+                            fprintf(stderr, "send return %ld", res);
+                            myerror(" ");
+                            break;
+                        }
+                    }
+                }
+            }
 		}
 	}
 	printf("disconnected\n");
 	fflush(stdout);
+    *sd_i-=1;
+    for (int i=0; i<MAX_CLIENT; i++) {
+        if(sd_arr[i] == sd) {
+            sd_arr[i] = -1;
+        }
+    }
 	::close(sd);
 }
 
@@ -148,15 +189,25 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+    int sd_arr[MAX_CLIENT] = {};
+    int sd_i = 0;
+
+    for(int i=0; i<MAX_CLIENT; i++) {
+        sd_arr[i] = -1;
+    }
+
 	while (true) {
 		struct sockaddr_in addr;
-		socklen_t len = sizeof(addr);
-		int newsd = ::accept(sd, (struct sockaddr *)&addr, &len);
+        socklen_t len = sizeof(addr);
+        int newsd = ::accept(sd, (struct sockaddr *)&addr, &len);
+        //왜 새로 커넥트하면 newsd가 4 부터 시작? 그리고 4, 5 가 연결되고 5를 디스커넥트 하고 다시 연결하면 6이 연결.
+        // 6을 끊고 다시 연결하면 5로 연결됨
+
 		if (newsd == -1) {
 			myerror("accept");
 			break;
 		}
-		std::thread* t = new std::thread(recvThread, newsd);
+		std::thread* t = new std::thread(recvThread, newsd, sd_arr, &sd_i);
 		t->detach();
 	}
 	::close(sd);
